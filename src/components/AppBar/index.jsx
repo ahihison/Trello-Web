639a5cc7d522fc70f48e92205700e6d8b28c984d
Sơn <88760761+ahihison@users.noreply.github.com>
2024-01-14 09:51:53 +0700

    Update AppBar styling and ModeSelect component

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -18,14 +18,14 @@ import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
 import Profile from './Menus/Profile'
 function AppBar() {
   return (
-    <Box sx={{
+    <Box px={2} sx={{
       width: '100%',
-      height: (theme) => theme.trello.boardBarHeight,
+      height: (theme) => theme.trello.appBarHeight,
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'space-between',
-      padding:2
-
+      gap:2,
+      overflowX:'auto'
     }}>
       <Box sx={{ display:'flex', alignItems:'center', gap:2 }}>
         <AppsIcon sx={{ color:'primary.main' }}/>
@@ -34,14 +34,17 @@ function AppBar() {
           <SvgIcon component={TrelloIcon} inheritViewBox fontSize="small" sx={{ color:'primary.main' }} />
           <Typography variant='span' sx={{ fontSize:'1.2rem', fontWeight:'bold', color:'primary.main' }}>Trello</Typography>
         </Box>
-        <WorkSpaces/>
-        <Recent/>
-        <Starred/>
-        <More/>
-        <Button variant="outlined">Create</Button>
+        <Box sx={{ display:{ xs:'none', md:'flex' }, gap:1 }}>
+          <WorkSpaces/>
+          <Recent/>
+          <Starred/>
+          <More/>
+          <Button variant="outlined">Create</Button>
+        </Box>
+
       </Box>
       <Box sx={{ display:'flex', alignItems:'center', gap:2 }}>
-        <TextField id="outlined-search" label="Search..." type="search" size="small"/>
+        <TextField id="outlined-search" label="Search..." type="search" size="small" sx={{ minWidth:'120px' }}/>
         <ModeSelect />
 
         <Tooltip title="Notification">
