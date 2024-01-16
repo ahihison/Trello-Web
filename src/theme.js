249edba468c27d8ed01fb9ecca0860702032b677
Sơn <88760761+ahihison@users.noreply.github.com>
2024-01-16 23:26:29 +0700

    ui boad content

--- src/theme.js
+++ src/theme.js
@@ -1,10 +1,13 @@
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
-
+const APP_BAR_HEIGHT = '58px'
+const BOARD_BAR_HEIGHT = '60px'
+const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`
 // Create a theme instance.
 const theme = extendTheme({
     trello: {
-        appBarHeight: '58px',
-        boardBarHeight: '60px'
+        appBarHeight: APP_BAR_HEIGHT,
+        boardBarHeight: BOARD_BAR_HEIGHT,
+        boardContentHeight: BOARD_CONTENT_HEIGHT
     },
     colorSchemes: {
         // light: {
@@ -51,9 +54,15 @@ const theme = extendTheme({
                 }
             }
         },
-        MuiInputLabel: {
+
+        MuiTypography: {
             styleOverrides: {
-                root: { fontSize: '0.875rem' }
+                root: {
+                    '&.MuiTypography-body1': {
+                        fontSize: '0.875rem'
+
+                    }
+                }
             }
         },
         MuiOutlinedInput: {
