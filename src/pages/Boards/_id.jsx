a6481aa7a9ae7646bf6dbe0a6598184e039efaa2
Sơn <88760761+ahihison@users.noreply.github.com>
2024-02-21 21:23:35 +0700

    Update API_ROOT and add axios dependency

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,13 +3,24 @@ import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import { mockData } from '~/apis/mock-data'
+import { fetchBoardDetailAPI } from '~/apis'
+import { useEffect, useState } from 'react'
 function Board() {
+  const [board, setBoard] = useState(null)
+  useEffect(() => {
+    const fetchBoardDetail = async () => {
+      const boardId = '65d5b7d9cf379313558f59ad'
+      const board = await fetchBoardDetailAPI(boardId)
+      setBoard(board)
+    }
+    fetchBoardDetail()
+  }, [])
   return (
     <>
       <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
         <AppBar/>
-        <BoardBar board ={mockData?.board}/>
-        <BoardContent board = {mockData?.board}/>
+        <BoardBar board ={board}/>
+        <BoardContent board = {board}/>
       </Container>
     </>
   )
