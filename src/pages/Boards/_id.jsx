3f9a5b663429729e509a6d082e17475692dcad84
Sơn <88760761+ahihison@users.noreply.github.com>
2024-02-22 21:24:49 +0700

    Refactor BoardBar component and add Tooltip

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,15 +1,14 @@
 import Container from '@mui/material/Container'
+import { useEffect, useState } from 'react'
+import { fetchBoardDetailAPI } from '~/apis'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
-import { mockData } from '~/apis/mock-data'
-import { fetchBoardDetailAPI } from '~/apis'
-import { useEffect, useState } from 'react'
 function Board() {
   const [board, setBoard] = useState(null)
   useEffect(() => {
     const fetchBoardDetail = async () => {
-      const boardId = '65d5b7d9cf379313558f59ad'
+      const boardId = '65d6bb4811db20c610affca6'
       const board = await fetchBoardDetailAPI(boardId)
       setBoard(board)
     }
