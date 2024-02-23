1aae770f6abfefc450668c1d183209aab405b172
Sơn <88760761+ahihison@users.noreply.github.com>
2024-02-23 09:59:39 +0700

    Update Header and Hero components

---
+++ src/components/Home/IntroduceTrello.jsx
@@ -0,0 +1,12 @@
+const IntroduceTrello = () => {
+  return (
+    <div className="flex gap-4 flex-col text-black mt-10">
+      <div>Trello 101</div>
+      <h2 className="text-2xl font-bold">A productivity powerhouse</h2>
+      <p className="line-clamp-4">Simple, flexible, and powerful. All it takes are boards, lists, and cards to get a clear view of who’s doing what and what needs to get done. Learn more in our
+        <a href="https://trello.com/guide" target="_blank" className="text-blue-700 underline" rel="noreferrer"> guide for getting started.</a></p>
+    </div>
+  )
+}
+
+export default IntroduceTrello
